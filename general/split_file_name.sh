dbe0a75438fb4a69c630bab5c80b5b08bb750f6b
saltypretzel303 <nemanja.m.303@gmail.com>
2022-05-26 02:16:38 +0200

    updated split_file_name.sh

--- general/split_file_name.sh
+++ general/split_file_name.sh
@@ -1,9 +1,17 @@
 #!/bin/bash
 
-FILE_NAME="some_file.txt.tar"
+FULL_PATH="./some/path/some_file.txt.tar"
+echo "Full path: $FULL_PATH"
+
+FILE_NAME=$(basename ${FULL_PATH})
+
+echo "Just a name: $FILE_NAME"
+
+echo "Just some parts of it ... "
 
 echo "${FILE_NAME%%.*}"
 echo "${FILE_NAME%.*}"
 
 echo "${FILE_NAME#*.}"
 echo "${FILE_NAME##*.}"
+
