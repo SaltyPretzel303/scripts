e2e663661b551f245c01ebe26e15c587eccccea1
saltypretzel303 <nemanja.m.303@gmail.com>
2022-07-14 14:55:05 +0200

    update youtube-download script with the note about yt-dlp, add config for the custom (more readable) docker ps format

--- general/get_yt_list.py
+++ general/get_yt_list.py
@@ -6,15 +6,23 @@ from os.path import exists
 import youtube_dl
 import sys
 
+# NOTE 
+# after all this script is actyally useless
+# instead of youtube_dl you should use yt_dlp (the main reason is that max download
+# speed is gonna be aroung 17Kb/s or somehing very slow)
+# to download video just use 
+# yt-dlp --no-playlist https://youtube...videhash
+# or just follow the docs for more options 
+
 LINKS_LIST = "./link_list"
 DOWNLOAD_DIR = "./new"
 
 # setup cli arguments parser
 
 parser = argparse.ArgumentParser(
-    "Downloads songs from youtube links specified as lines of file. \n\
-    Default links list is ./link_list file.\n\
-    Defualt destination dir of downloaded songs is ./new")
+    f"Downloads songs from youtube links specified as lines of file. \n\
+    Default links list is {LINKS_LIST} file.\n\
+    Defualt destination dir of downloaded content is {DOWNLOAD_DIR} .\n")
 
 parser.add_argument("--links",
                     dest="links_list",
@@ -29,10 +37,12 @@ if(hasattr(cli_input, "links_list") and
         (cli_input.links_list is not None) and (cli_input.links_list != "")):
 
     links_list = cli_input.links_list
-    print("Using provided links list on: " + links_list)
+    
 else:
     links_list = LINKS_LIST
-    print("Using default links list on: " + links_list)
+
+
+print(f"Using links from the path: {links_list} ... ")
 
 # download specified links
 
