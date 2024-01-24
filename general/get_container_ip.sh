d1412c08d173678358e117f1c660ed1e399330e3
saltypretzel303 <nemanja.m.303@gmail.com>
2024-01-24 16:55:41 +0100

    add support for stdin and xargs, also -a flag will print names as well (user docker ps --format "{{.Names}}" to list all running container names)

--- general/get_container_ip.sh
+++ general/get_container_ip.sh
@@ -1,11 +1,47 @@
 #!/bin/bash
 
-if [ $# != 1 ]
+with_name='{{.Name}}:  '
+
+name_arg=''
+all_flag='-a'
+
+all_names=
+
+function chec_all_flag {
+	for arg in $@
+	do 
+		if [[ "$arg" == "$all_flag" ]]
+		then 
+			name_arg=$with_name
+			return
+		fi 
+	done 
+}
+
+chec_all_flag $@
+
+# -t check if current input is terminal.
+# if ! -t there is input piped from other command. 
+if [ ! -t 0 ]
 then 
-	echo "Please provide container name as the first (and only) argument."
 
-	exit 1
-fi 
+	while read line
+	do
+		all_names+=" $line"
+	done 
 
-docker inspect "$1" --format "{{range .NetworkSettings.Networks}}{{.IPAddress}}{{end}}"
+	# This should replace the above alg but not sure how it (dosn't) works.
+	# readarray -t read_names
+fi
 
+all_names+=" $@"
+
+for c_name in $all_names
+do
+	if [[ "$c_name" == "$all_flag" ]]
+	then 
+		continue
+	fi
+
+	docker inspect "$c_name" --format "$name_arg{{range .NetworkSettings.Networks}}{{.IPAddress}}{{end}}"
+done
